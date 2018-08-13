3018e5c8d0c0cfd9b1eaa837d5359d1b922ca181
代云舟 <1254473705@qq.com>
2018-08-14 00:05:06 +0800

    更新

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="home">
-    <img src="@/assets/logo.png">
+   
+    <el-container>
+      <el-header>
+        頭部
+      </el-header>
+      <el-main>
+        內容
+         <img src="@/assets/logo.png">
+      </el-main>
+      <el-footer>
+        <FooterBar focus="Home"/>
+      </el-footer>
+       
+    </el-container>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <FooterBar focus="Home"/>
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
-import FooterBar from "@/components/FooterBar.vue";
-
+import FooterBar from '@/components/FooterBar.vue';
 
 export default {
-  name: 'home',
-  components: {
-    FooterBar,
-  }
-}
+	name: 'home',
+	components: {
+		FooterBar,
+	},
+};
 </script>
