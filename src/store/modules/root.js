b665d4e49e39a296c42568ef5d9d236ba1b752a1
代云舟 <1254473705@qq.com>
2018-08-13 23:35:50 +0800

    添加路由对应页面

---
+++ src/store/modules/root.js
@@ -0,0 +1,40 @@
+/**
+ * 公共的状态管理
+ * store.state.root
+ */
+
+// initial state
+const state = {
+    items: []
+}
+
+//getters 
+const getters = {
+    //默认getters模板
+    defaultGetters(){
+
+    }
+};
+
+//actions 
+const actions = {
+    //默认actions模板
+    defaultActions({commit,state,rootState}){
+        commit('defaultMutations', state.items);
+    }
+};
+//mutations
+const mutations = {
+    //默认mutations模板
+    defaultMutations(state,data){
+        console.log(data);
+    }
+};
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+  }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,6 @@
     <img src="@/assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <FooterBar focus="Home"/>
-    <TestVuex  msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -11,15 +10,12 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue';
 import FooterBar from "@/components/FooterBar.vue";
-import TestVuex from "@/components/TestVuex.vue";
 
 
 export default {
   name: 'home',
   components: {
-    // HelloWorld,
     FooterBar,
-    TestVuex
   }
 }
 </script>
